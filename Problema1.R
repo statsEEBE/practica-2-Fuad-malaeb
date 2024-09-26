0c126c375cb3e4e3b9b34cf6f51c011474d53712
fuad.malaeb <fuad.malaeb@auleseebe.upc.edu>
2024-09-26 11:28:27 +0200

    prob 1

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,9 @@
 #Codigo para problema 1
 
+# la pregunta 1 seria trobar la m de la funcio que seria el 0.06576
+
+#coef de correlacio, el qual es la arrel cuadrada del multiple R-squared
+sqrt(0.1923) # resposta pregunta 2
+
+0.06576* 647.1+ 11.79755 # resposta de la pregunta 4
+
